782d97943657c8a9e6c805755b7602f911821553
Kiran Wijesooriya <kiranw1000@gmail.com>
2025-07-23 10:05:49 -0400

    Add checks to conversion scripts

--- DTU/conversion.py
+++ DTU/conversion.py
@@ -3,9 +3,22 @@ import scipy.io as sio
 import pandas as pd
 import numpy as np
 import soundfile as sf
+import scipy.signal as signal
 from collation import split_eeg
 
 def main(args):
+    # Print configuration
+    print("Data Directory:", args.data_dir)
+    print("Output Directory:", args.output_dir)
+    print("Sample Spacing:", args.sample_spacing)
+    print("Sample Length:", args.sample_length)
+    print("Validation Subjects:", args.val_subjects)
+    print("Test Subjects:", args.test_subjects)
+    print("Low Cutoff Frequency:", args.low_cutoff)
+    print("High Cutoff Frequency:", args.high_cutoff)
+    print("Resampling Frequency:", args.resample_freq)
+    
+    # Check and set directories
     assert args.data_dir != ".", "Please provide a valid data directory."
     os.chdir(args.data_dir)
     print("Current working directory:", os.getcwd())
@@ -24,20 +37,48 @@ def main(args):
     shutil.copytree("AUDIO", os.path.join(args.output_dir, "AUDIO"), dirs_exist_ok=True)
     print("Copied AUDIO directory to output directory.")
 
+    filtering =  args.high_cutoff is not None or args.low_cutoff is not None
+    
+    resampling = args.resample_freq is not None
+
     output = []
     split = "train"
-    for i, subject in tqdm.tqdm(enumerate(subjects)):
+    for i, subject in tqdm.tqdm(enumerate(subjects), total=num_subjects, desc="Processing subjects", position=0, leave=True):
         if i > num_subjects - args.test_subjects:
             split = "test"
         elif i > num_subjects - args.test_subjects - args.val_subjects:
             split = "val"
+        # Load experiment info and EEG data
         expinfo = pd.read_csv(os.path.join(args.data_dir, 'EEG', f"{subject}.csv"))
         mat = sio.loadmat(os.path.join(args.data_dir, "EEG", f"{subject}.mat"), squeeze_me=True, struct_as_record=False)
         wav_files = set([x for x in expinfo[["wavfile_male", "wavfile_female"]].values.flatten() if type(x) == str])
         wav_files = [(sf.read(os.path.join(args.data_dir, "AUDIO", x))) for x in wav_files]
         wavMinLength = min([x[0].shape[0] / x[1] for x in wav_files if x[0] is not None])
+        # Split EEG data into trials
         eeg_data, indices = split_eeg(mat, expinfo, wavMinLength=wavMinLength)
+        # Filter and resample EEG data if specified
+        fs = mat["data"].fsample.eeg
+        if filtering:
+            low = args.low_cutoff / fs
+            high = args.high_cutoff / fs
+            if low <= 0:
+                ftype = 'lowpass'
+                cutoffs = [high]
+            elif high >= 1:
+                ftype = 'highpass'
+                cutoffs = [low]
+            else:
+                ftype = 'bandpass'
+                cutoffs = [low, high]
+            print(f"Filtering EEG data with {ftype} filter: {cutoffs}")
+            sos = signal.butter(4, cutoffs, btype=ftype, output='sos')
+            eeg_data = signal.sosfiltfilt(sos, eeg_data, axis=1)
+        if resampling:
+            resampled_size = int(args.resample_freq * eeg_data.shape[1] / fs)
+            eeg_data = signal.resample(eeg_data, resampled_size, axis=1)
+            print(f"EEG data shape after resampling: {eeg_data.shape}")
         expinfo = expinfo.iloc[indices]
+        # Populate mix file which specifies audio and EEG data correspondence
         for trial in range(eeg_data.shape[0]):
             wav_m, wav_f = expinfo.iloc[trial][["wavfile_male", "wavfile_female"]].values
             if expinfo.iloc[trial]["attend_mf"] == 1:
@@ -59,6 +100,9 @@ if __name__ == "__main__":
     parser.add_argument("--sample_length", type=int, default=10, help="Length of each EEG sample in seconds")
     parser.add_argument("--val_subjects", type=int, default=2, help="Number of subjects for validation")
     parser.add_argument("--test_subjects", type=int, default=2, help="Number of subjects for testing")
+    parser.add_argument("--high_cutoff", type=float, default=None, help="High cutoff frequency for filtering")
+    parser.add_argument("--low_cutoff", type=float, default=None, help="Low cutoff frequency for filtering")
+    parser.add_argument("--resample_freq", type=int, default=None, help="Resampling frequency for EEG data")
 
     args = parser.parse_args()
 
--- DTU/conversion.sh
+++ DTU/conversion.sh
@@ -8,19 +8,40 @@ conda activate aspire
 
 #####
 # Modify these lines
-data_dir="."
-output_dir="./DTU"  # Directory to save the converted data
+data_dir=$(pwd)
+output_dir=$(pwd)/DTU  # Directory to save the converted data
 sample_spacing=1
 sample_length=10
 val_subjects=2
 test_subjects=2
+low_cutoff=None  # Low cutoff frequency for filtering (Hz)
+high_cutoff=None  # High cutoff frequency for filtering (Hz)
+resample_freq=128  # Resampling frequency (Hz), if None, no resampling is done
 #####
-echo data_dir: $data_dir
-echo output_dir: $output_dir
-echo sample_spacing: $sample_spacing
-echo sample_length: $sample_length
-echo val_subjects: $val_subjects
-echo test_subjects: $test_subjects
+
+# Populate optional args
+args=""
+if [ "$low_cutoff" != "None" ]; then
+    args="$args --low_cutoff $low_cutoff"
+fi
+if [ "$high_cutoff" != "None" ]; then
+    args="$args --high_cutoff $high_cutoff"
+fi
+if [ "$sample_spacing" != "None" ]; then
+    args="$args --sample_spacing $sample_spacing"
+fi
+if [ "$sample_length" != "None" ]; then
+    args="$args --sample_length $sample_length"
+fi
+if [ "$val_subjects" != "None" ]; then
+    args="$args --val_subjects $val_subjects"
+fi
+if [ "$test_subjects" != "None" ]; then
+    args="$args --test_subjects $test_subjects"
+fi
+if [ "$resample_freq" != "None" ]; then
+    args="$args --resample_freq $resample_freq"
+fi
 
 # Run the conversion script
-python conversion.py --data_dir "$data_dir" --output_dir "$output_dir" --sample_spacing "$sample_spacing" --sample_length "$sample_length" --val_subjects "$val_subjects" --test_subjects "$test_subjects"
+python conversion.py --data_dir "$data_dir" --output_dir "$output_dir" $args
