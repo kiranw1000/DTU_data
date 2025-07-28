764c61a57db064d15a5f94848c2e0e380ae70c0d
Kiran Wijesooriya <kiranw1000@gmail.com>
2025-07-28 16:09:51 -0400

    Adjust conversion to put start times in terms of seconds

--- DTU/conversion.py
+++ DTU/conversion.py
@@ -87,8 +87,8 @@ def main(args):
                 attn_wav, int_wav = wav_f, wav_m
             eeg_path = os.path.join(args.output_dir, "eeg", f"{subject}Tra{trial+1}.npy")
             np.save(eeg_path, eeg_data[trial])
-            time_to_sample = int(eeg_data[trial].shape[0] - (mat["data"].fsample.eeg * args.sample_length))
-            for j in range(0, time_to_sample, args.sample_spacing * mat["data"].fsample.eeg):
+            time_to_sample = int((eeg_data[trial].shape[0]/mat["data"].fsample.eeg)  - args.sample_length)
+            for j in range(0, time_to_sample, args.sample_spacing):
                 output.append([split, i + 1, trial + 1, attn_wav, j, 0, int_wav, j, 0, args.sample_length])
     np.savetxt(os.path.join(args.output_dir, "mix.csv"), output, fmt="%s", delimiter=",")
 
