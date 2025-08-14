1d131f841eb186728579b561909c8927e0345916
Kiran Wijesooriya <kiranw1000@gmail.com>
2025-08-14 16:41:13 -0400

    Add shuffling and disallow overlapping samples

--- DTU/conversion.py
+++ DTU/conversion.py
@@ -10,7 +10,6 @@ def main(args):
     # Print configuration
     print("Data Directory:", args.data_dir)
     print("Output Directory:", args.output_dir)
-    print("Sample Spacing:", args.sample_spacing)
     print("Sample Length Mean:", args.sample_length_mean)
     print("Sample Length Std:", args.sample_length_std)
     print("Validation Split:", args.val_split)
@@ -49,6 +48,7 @@ def main(args):
     output = pd.DataFrame(columns=["split", "subject", "trial", "tgt_audio", "tgt_start", "", "int_audio", "int_start", "snr", "length"])
     trials = []
     # Process each subject
+    distribution = np.random.Generator()
     for i, subject in tqdm.tqdm(enumerate(subjects[:num_subjects]), total=num_subjects, desc="Processing subjects", position=0, leave=True):
         # Load experiment info and EEG data
         expinfo = pd.read_csv(os.path.join(args.data_dir, 'EEG', f"{subject}.csv"))
@@ -95,8 +95,11 @@ def main(args):
             eeg_path = os.path.join(args.output_dir, "eeg", f"{subject}Tra{trial+1}.npy")
             np.save(eeg_path, eeg_data[trial])
             time_to_sample = int((eeg_data[trial].shape[0]/mat["data"].fsample.eeg)  - args.max_length)
-            for j in np.arange(0, time_to_sample, args.sample_spacing):
-                output = pd.concat([output, pd.DataFrame([["",i+1, trial+1, attn_wav, j, "", int_wav, j, 0, args.sample_length_mean]], columns=output.columns)])
+            j = 0
+            while j < time_to_sample:
+                sample_length = temp (temp:= distribution.normal(args.sample_length_mean, args.sample_length_std)) if j+temp < time_to_sample else time_to_sample - j
+                output = pd.concat([output, pd.DataFrame([["",i+1, trial+1, attn_wav, j, "", int_wav, j, 0, sample_length]], columns=output.columns)])
+                j += sample_length
     num_trials = len(trials)
     val_trials = math.floor(args.val_split * num_trials)
     test_trials = math.floor(args.test_split * num_trials)
@@ -111,9 +114,8 @@ def main(args):
     output.reset_index(drop=True, inplace=True)
     for i, row in tqdm.tqdm(output.iterrows(), desc="Assigning splits", position=2, leave=False):
         output.at[i, "split"] = trial_dict[(row["subject"], row["trial"])]
-    if args.sample_length_std > 0:
-        output["length"] = np.random.normal(args.sample_length_mean, args.sample_length_std, size=len(output))
-        output["length"] = output["length"].clip(lower=args.min_length, upper=args.max_length)
+        
+    output = output.sample(frac=1).reset_index(drop=True)
 
     output.to_csv(os.path.join(args.output_dir, "mix.csv"), index=False, header=False)
 
@@ -121,7 +123,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="DTU Data Conversion")
     parser.add_argument("--data_dir", type=str, required=True, help="Directory containing DTU data")
     parser.add_argument("--output_dir", type=str, required=True, help="Directory to save converted data")
-    parser.add_argument("--sample_spacing", type=float, default=1, help="Sample spacing for EEG data")
     parser.add_argument("--sample_length_mean", type=float, default=5.5, help="Mean length of each EEG sample in seconds")
     parser.add_argument("--sample_length_std", type=float, default=0, help="Standard deviation of each EEG sample length in seconds")
     parser.add_argument("--max_length", type=float, default=10, help="Maximum length of audio samples in seconds")
